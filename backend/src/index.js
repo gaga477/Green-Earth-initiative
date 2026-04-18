ad4e45aa295e5969a0565ea90517a8f6d89d61ad
Ogaga Ejairu <ejairuogaga@gmail.com>
2026-04-18 12:23:57 -0700

    Add root API status route

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use("/api", userRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ msg: err.message || "Something went wrong" });
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
